cb786a1f7aeda72f8794f3fad634124cd6295a0f
Tom Jenkinson <tom@tjenkinson.me>
2022-05-26 21:43:23 +0100

    simplify atomic group handling
    not sure what I was thinking before it was over complicated a lot

---
+++ src/sets.test.ts
@@ -0,0 +1,55 @@
+import { areSetsEqual, mergeSets, setsOverlap, subtractSets } from './sets';
+
+describe('Sets', () => {
+  describe('areSetsEqual', () => {
+    it('works', () => {
+      expect(areSetsEqual(new Set(), new Set())).toBe(true);
+      expect(areSetsEqual(new Set([1]), new Set([]))).toBe(false);
+      expect(areSetsEqual(new Set([]), new Set([1]))).toBe(false);
+      expect(areSetsEqual(new Set([1]), new Set([1]))).toBe(true);
+      expect(areSetsEqual(new Set([1, 2]), new Set([1, 3]))).toBe(false);
+      expect(areSetsEqual(new Set([2, 1]), new Set([1, 2]))).toBe(true);
+    });
+  });
+
+  describe('mergeSets', () => {
+    it('works', () => {
+      expect([...mergeSets(new Set(), new Set())]).toStrictEqual([]);
+      expect([...mergeSets(new Set([1]), new Set([2]))].sort()).toStrictEqual([
+        1, 2,
+      ]);
+      expect(
+        [...mergeSets(new Set([1, 2]), new Set([2]))].sort()
+      ).toStrictEqual([1, 2]);
+      expect(
+        [...mergeSets(new Set([1, 2]), new Set([2, 3]))].sort()
+      ).toStrictEqual([1, 2, 3]);
+    });
+  });
+
+  describe('subtractSets', () => {
+    it('works', () => {
+      expect([...subtractSets(new Set(), new Set())]).toStrictEqual([]);
+      expect(
+        [...subtractSets(new Set([1]), new Set([2]))].sort()
+      ).toStrictEqual([1]);
+      expect(
+        [...subtractSets(new Set([1, 2]), new Set([2]))].sort()
+      ).toStrictEqual([1]);
+      expect(
+        [...subtractSets(new Set([1, 2]), new Set([2, 3]))].sort()
+      ).toStrictEqual([1]);
+    });
+  });
+
+  describe('setsOverlap', () => {
+    it('works', () => {
+      expect(setsOverlap(new Set(), new Set())).toBe(false);
+      expect(setsOverlap(new Set([1]), new Set([]))).toBe(false);
+      expect(setsOverlap(new Set([]), new Set([1]))).toBe(false);
+      expect(setsOverlap(new Set([1]), new Set([1]))).toBe(true);
+      expect(setsOverlap(new Set([1, 2]), new Set([1, 3]))).toBe(true);
+      expect(setsOverlap(new Set([2, 1]), new Set([1, 2]))).toBe(true);
+    });
+  });
+});
---
+++ src/sets.ts
@@ -0,0 +1,22 @@
+export function areSetsEqual<T>(a: ReadonlySet<T>, b: ReadonlySet<T>): boolean {
+  if (a.size !== b.size) return false;
+  for (const entry of a) {
+    if (!b.has(entry)) return false;
+  }
+  return true;
+}
+
+export function mergeSets<T>(a: ReadonlySet<T>, b: ReadonlySet<T>): Set<T> {
+  return new Set([...a, ...b]);
+}
+
+export function subtractSets<T>(a: ReadonlySet<T>, b: ReadonlySet<T>): Set<T> {
+  const newSet = new Set(a);
+  b.forEach((bEntry) => newSet.delete(bEntry));
+  return newSet;
+}
+
+export function setsOverlap<T>(a: ReadonlySet<T>, b: ReadonlySet<T>): boolean {
+  const total = a.size + b.size;
+  return new Set([...a, ...b]).size < total;
+}
