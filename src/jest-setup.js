74c9eb9cc31c8399289de7aab22ef509660c0d5a
Tom Jenkinson <tom@tjenkinson.me>
2022-08-15 21:07:35 +0300

    update comment and link to PR with fix

--- src/jest-setup.js
+++ src/jest-setup.js
@@ -2,6 +2,8 @@ afterAll(() => {
   // prevents
   // "A worker process has failed to exit gracefully and has been force exited. This is likely caused by tests leaking due to improper teardown. Try running with --detectOpenHandles to find leaks. Active timers can also cause this, ensure that .unref() was called on them."
   // happening
-  // My theory is without the forced GC the `FinalisationRegistry` we use hasn't run for everything yet and jest thinks the `heldValue`'s registered there are leaks
+  // This seems to be because the process is taking too long to terminate, as a result of GC taking too long during the timeout period
+  // Doing this moves the GC to before the request for the process to stop where the timeout is running
+  // See https://github.com/facebook/jest/pull/13139
   global.gc?.();
 });
