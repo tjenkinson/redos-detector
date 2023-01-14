182208a8f1c9dc59d9237caab6fb0cf13f475415
Tom Jenkinson <tom@tjenkinson.me>
2023-01-14 22:34:21 +0000

    update jest

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: 'src',
   restoreMocks: true,
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-  globals: {
-    'ts-jest': {
-      tsconfig: path.resolve(__dirname, './tsconfig.test.json'),
-    },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: path.resolve(__dirname, './tsconfig.test.json'),
+      },
+    ],
   },
   coverageThreshold: {
     global: {
